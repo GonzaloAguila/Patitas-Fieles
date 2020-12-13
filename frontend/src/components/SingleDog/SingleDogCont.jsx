b2e8b253ad8be8f7dc4adbb6e6e31a43aa4d2ae7
Gonzalo Aguila <aguilagonzalo1@gmail.comgit commit -m testing>
2020-12-13 17:33:36 -0300

    asd

--- frontend/src/components/SingleDog/SingleDogCont.jsx
+++ frontend/src/components/SingleDog/SingleDogCont.jsx
@@ -18,26 +18,24 @@ const SingleDogCont = ({history, match}) => {
   const dispatch = useDispatch()
 
   useEffect(() => {
-    console.log('entre al effect')
     dispatch(fetchDogs()).then(() => {
       dispatch(fetchDog(match.params.dogname))
     })
   }, []); 
+
+  useEffect(() => {
+    history.push(`/adoptar/${selectedDog.name}`)
+  }, [selectedDog])
 
    //Array circular para ir cambiando de perros, se lo paso al dummy
    const nextDog = (e,name) => {
        e.preventDefault()
        let dogNames = dogs.map((dog) => dog.name)//array de nombres ordenados
        let currentDogIndex= dogNames.indexOf(name)//0
-       console.log(currentDogIndex, 'index')
        currentDogIndex < dogNames.length-1 
        ? currentDogIndex = currentDogIndex+1 
        : currentDogIndex = 0;
        dispatch(fetchDog(dogNames[currentDogIndex]))
-       .then(() => {
-           history.push(`/adoptar/${selectedDog.name}`)
-       })
-       return 
    }
   
   return (
