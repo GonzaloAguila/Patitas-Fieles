d4773397d85aa9f7c0c84932e9d9b048af5f00a2
GonzaloAguila <aguilagonzalo1@gmail.com>
2021-03-25 12:18:43 -0300

    Update index.js

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 
-const URI = "mongodb+srv://gonzalo:14781716asd@cluster0prueba.cz9qc.mongodb.net/patitas?retryWrites=true&w=majority"
+const URI = "mongodb+srv://@cluster0prueba.cz9qc.mongodb.net/patitas?retryWrites=true&w=majority"
 
 const connectDB = async ()=> {
   await mongoose.connect(URI, {useUnifiedTopology: true, useNewUrlParser: true })
